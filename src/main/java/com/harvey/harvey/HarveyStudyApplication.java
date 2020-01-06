ba18be16d907bc125990165d237dd5bb0f516d61
harvey <249057029@qq.com>
2020-01-06 14:50:52 +0800

    init project

---
+++ src/main/java/com/harvey/harvey/HarveyStudyApplication.java
@@ -0,0 +1,14 @@
+package com.harvey.harvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarveyStudyApplication{
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HarveyStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harvey/harvey/HarveyStudyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.harvey.harvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarveyStudyApplicationTests{
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
