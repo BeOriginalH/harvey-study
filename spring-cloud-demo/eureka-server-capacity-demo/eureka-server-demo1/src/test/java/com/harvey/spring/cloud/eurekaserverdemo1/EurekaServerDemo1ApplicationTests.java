f5aa5d5a33342f286a09a644458ee4101023d6f2
hsy <249057029@qq.com>
2021-01-05 08:16:23 +0800

    d

---
+++ spring-cloud-demo/eureka-server-capacity-demo/eureka-server-demo1/src/test/java/com/harvey/spring/cloud/eurekaserverdemo1/EurekaServerDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harvey.spring.cloud.eurekaserverdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
